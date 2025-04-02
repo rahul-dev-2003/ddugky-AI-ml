9e1bfc0e49a05ad5153ff3439dedb4a278e8b63b
Rahul Garwal <rahul.garwal.2003@gmail.com>
2025-04-02 14:26:29 +0530

    List data types find value ,find the element length,accsess value by index

---
+++ Python-Fundamentals/Lists.py
@@ -0,0 +1,10 @@
+# List In python
+# string - inmmutable list - mutable
+
+marks = [94.5, 87.5,95.55,66.4,455.1]
+print(marks)
+print(type(marks))
+print(len(marks))
+print(marks[0])
+print(marks[1])
+
--- Python-Fundamentals/condiitional_statement.py
+++ Python-Fundamentals/condiitional_statement.py
@@ -1,4 +1,5 @@
 # if - elseif-else(syntax)
+
 age =21
 if(age >=18):
     print("you are eligibale to voat")
@@ -17,5 +18,28 @@ elif(light == "green"):
 elif(light == "yellow"):
     print("ready")
 else:
-    print("wrong signal")        
+    print("wrong signal")    
+
+# Student marks
+marks = int(input("Enter your marks"))
+if(marks>90 & marks>80):
+    print("A")
+elif(marks>80 & marks>60):
+    print("A")
+elif(marks>60 & marks>40):
+    print("A")
+elif(marks>40 & marks>30):
+    print("pass")
+elif(marks>30 & marks>25):
+    print("fail")
+else:
+    print("invalid number")        
+
+
+    
+
+        
+
+
+
       
