5fdcc68c183e39e548b2b269ba80270dc57cc511
Rahul Garwal <rahul.garwal.2003@gmail.com>
2025-04-02 21:53:31 +0530

    nested if

--- Python-Fundamentals/condiitional_statement.py
+++ Python-Fundamentals/condiitional_statement.py
@@ -35,6 +35,44 @@ elif(marks>30 & marks>25):
 else:
     print("invalid number")        
 
+######3#### nested if  conddition
+num = 10
+if(num > 0):
+    print("number is positive")
+    if(num % 2==0):
+        print("number is even")
+    else:
+        print("number is odd")
+else:
+    print("number is not positive")
+
+
+######3#####3###########3#3#######3#3##############3#
+# compare of 3 numbers
+num1 = int(input("enter first number"))
+num2 = int(input("enter second number"))
+num3 = int(input("enter third number"))
+
+if(num1>num2):
+    if(num1>num3):
+        print(num1,"num1 is gretest")
+    else:
+        print(num3,"num 3 gretest")
+elif(num2>num3):
+    print(num2,"num 2 is gretest")
+else:
+    print(num3," num 3 is gretest")    
+
+# # number clssification
+num1 = int(input("enter number"))
+if(num1>0):
+    if(num1%2==0):
+        print(" number is even")
+    else:
+        print("number is nagative")  
+
+#################################################              
+        
 
     
 
