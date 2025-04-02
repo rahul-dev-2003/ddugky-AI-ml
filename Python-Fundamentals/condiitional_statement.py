10da1ecbf5678430a4a2ea76b3559292c7f8a037
Rahul Garwal <rahul.garwal.2003@gmail.com>
2025-04-02 12:59:48 +0530

    operators and conditional operation

---
+++ Python-Fundamentals/condiitional_statement.py
@@ -0,0 +1,21 @@
+# if - elseif-else(syntax)
+age =21
+if(age >=18):
+    print("you are eligibale to voat")
+    print("you can drive")
+else:
+    print("you are not eligible to boat")
+    print("you can not drive")  
+
+
+# elif 
+light= "red"
+if(light == "red"):
+    print("stop")
+elif(light == "green"):
+    print("go")
+elif(light == "yellow"):
+    print("ready")
+else:
+    print("wrong signal")        
+      
