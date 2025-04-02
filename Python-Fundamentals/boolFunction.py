73cb75bbbff4d9145825a189f74c098afd707499
Rahul Garwal <rahul.garwal.2003@gmail.com>
2025-04-02 13:00:17 +0530

    operators and conditional

---
+++ Python-Fundamentals/boolFunction.py
@@ -0,0 +1,12 @@
+# the bool() function allow you to evalute any value , 
+# and give you true or false in return.
+bool("abc")
+bool(123)
+bool(["apple","cherry","banana"])
+
+print(bool("abc"))
+print(bool(123))
+print(bool(["apple","cherry","banana"]))
+
+print (bool("hello"))
+print(bool(1.2))
---
+++ Python-Fundamentals/comparition.py
@@ -0,0 +1,65 @@
+a=50
+b=20
+print(a==b) # false
+print(a!=b) # True
+print(a>b)# true
+print(a>=b) # true
+print(a<=b) #false
+
+# Assignnment operarator
+
+x=5
+print(x)
+num=50
+num //=8
+
+print(num)
+#num = num+10 +10=20
+num=50
+# num1+= 10
+
+num2= 40
+# num2*=5
+
+num3=40
+# num3-=5
+
+num4=40
+num4/=5
+
+num5=40
+# num5**=5
+
+num6=66
+# num6%=5
+
+num7=60
+# num7//=5
+
+# print("num : ",num1)
+# print("num2 :",num2 )
+# print("num2 :",num3 )
+# print("num2 :",num4 )
+
+# print("num2 :",num5 )
+
+# print("num2 :",num6 )
+# print("num2 :",num7 )
+
+# logical operator 
+a= 50
+b=38
+print(not True)
+print(not False)
+print(not(a>b))
+print(not(a<b))
+
+# and; onne is fakse then false
+val1=False
+val2=True
+print("and operator",val1 and vars)
+
+# or one is true then true
+
+print("or operator",val1 or val2)
+print("or operator",(a==b) or (a>b))
